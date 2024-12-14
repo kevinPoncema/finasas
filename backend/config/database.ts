8cbbaee707ea888de56834f87c934b0bbafa0a98
kevin ponce <kevin.ponce@ieesa.edu.hn>
2024-12-14 17:54:07 -0600

    cambio de orm

---
+++ backend/config/database.ts
@@ -0,0 +1,18 @@
+import { Sequelize } from "sequelize-typescript";
+import dotenv from "dotenv";
+dotenv.config();
+
+import path from "path";
+
+const sequelize = new Sequelize({
+  dialect: "mysql",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME, 
+  models: [path.join(__dirname, "../models")], // Ruta a los modelos
+  logging: console.log, // Puedes desactivar los logs con `false`
+});
+
+export default sequelize;
