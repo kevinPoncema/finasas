698a04aadffe71c5f0c0498bc01bf14c8b6603b4
kevin ponce <kevin.ponce@ieesa.edu.hn>
2024-12-24 16:21:34 -0600

    utilidad para jwt

---
+++ backend/src/utils/JWTManager.ts
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken';
+import dotenv from "dotenv";
+dotenv.config();
+
+export class JWTManager {
+  private static secretKey =process.env.TOKENT_SECRET_KEY; // Cambia esto por una clave segura
+  private static expiresIn = '1h'; // Cambia esto según el tiempo de expiración deseado
+
+  /**
+   * Crea un token JWT basado en el payload proporcionado.
+   * @param payload Objeto con los datos a incluir en el token.
+   * @returns Token JWT como cadena.
+   */
+
+  public static createToken(payload: object): string {
+    try {
+      const token = jwt.sign(payload, this.secretKey, { expiresIn: this.expiresIn });
+      return token;
+    } catch (error) {
+      throw new Error(`Error al crear el token: ${error.message}`);
+    }
+  }
+
+  /**
+   * Decodifica un token JWT y devuelve el objeto contenido en él.
+   * @param token Token JWT a decodificar.
+   * @returns Objeto contenido en el token.
+   */
+  public static decodeToken(token: string): object {
+    try {
+      const decoded = jwt.verify(token, this.secretKey);
+      return decoded as object;
+    } catch (error) {
+      throw new Error(`Error al decodificar el token: ${error.message}`);
+    }
+  }
+}
